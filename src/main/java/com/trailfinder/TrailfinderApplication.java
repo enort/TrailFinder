97299b786e06dfebfba4d6172f1f9f4b61a8d024
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-07 22:19:28 -0500

    Initial Commit

---
+++ src/main/java/com/trailfinder/TrailfinderApplication.java
@@ -0,0 +1,13 @@
+package com.trailfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailfinderApplication.class, args);
+	}
+
+}

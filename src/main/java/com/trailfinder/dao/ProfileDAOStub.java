a513391604556ccf35aaa5b3915100c336d12d4b
LookItsCashew <schallkd@mail.uc.edu>
2020-02-14 10:14:49 -0500

    Created Stub classes for mock data implementation/testing

---
+++ src/main/java/com/trailfinder/dao/ProfileDAOStub.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.trailfinder.dao;
+
+import com.trailfinder.dto.ProfileDTO;
+
+/**
+ * @author LookItsCashew
+ *
+ */
+public class ProfileDAOStub implements IProfileDAO {
+
+	@Override
+	public ProfileDTO getProfile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveProfile() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ProfileDTO viewProfile(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

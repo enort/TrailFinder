d459c02f1b1c80a68ae1ffa504bbffee31bd99de
LookItsCashew <schallkd@mail.uc.edu>
2020-02-20 23:14:53 -0500

    Implemented stub and test. Test not working

---
+++ src/main/java/com/trailfinder/service/HikeServiceStub.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package com.trailfinder.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.trailfinder.dto.EventDTO;
+import com.trailfinder.dto.ProfileDTO;
+import com.trailfinder.dto.TrailDTO;
+
+/**
+ * @author LookItsCashew
+ *
+ */
+public class HikeServiceStub  implements IHikeService {
+
+	@Override
+	public ProfileDTO getProfile(int profileId) {
+		// TODO create a List of profiles and check if the passed Id matches any
+		List<ProfileDTO> profiles = new ArrayList<ProfileDTO>();
+		// Create 5 profiles
+		int i;
+		for (i = 0; i < 5; i++)
+		{
+			ProfileDTO testProfile = new ProfileDTO();
+			testProfile.setProfileId(i);
+			testProfile.setFirstName("TestName" + i);
+			testProfile.setBio("TestBio");
+			testProfile.setEvents(null);
+			profiles.add(testProfile);
+		}
+		
+		// Check which profile was selected
+		ProfileDTO profile = new ProfileDTO();
+		switch (profileId) 
+		{
+			case 0: profile = profiles.get(0); break;
+			case 1: profile = profiles.get(1); break;
+			case 2: profile = profiles.get(2); break;
+			case 3: profile = profiles.get(3); break;
+			case 4: profile = profiles.get(4); break;
+		
+		}
+		
+		return profile;
+	}
+
+	@Override
+	public void saveProfile(ProfileDTO profile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<TrailDTO> getNearbyTrails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<EventDTO> getEvents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createEvent(EventDTO event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/com/trailfinder/tests/HikeServiceTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.trailfinder.tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.trailfinder.dto.ProfileDTO;
+import com.trailfinder.service.IHikeService;
+
+/**
+ * This is a test for the hike service class
+ * @author LookItsCashew
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class HikeServiceTest {
+	
+	@Autowired
+	IHikeService hikeServiceStub;
+	
+	ProfileDTO profile;
+	
+	@Test
+	public void getProfile_ReturnSelectedProfileUserExists() {
+		givenEventHasProfileOfCreator();
+		whenProfileIsSelected();
+		thenProfileReturnedIfExists();
+	}
+
+	private void givenEventHasProfileOfCreator() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void whenProfileIsSelected() {
+		// TODO Auto-generated method stub
+		profile = hikeServiceStub.getProfile(2);
+	}
+
+	private void thenProfileReturnedIfExists() {
+		// TODO Auto-generated method stub
+		assertTrue(profile != null);
+	}
+	
+}

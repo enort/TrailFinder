e3056a38f7bc2aed30fb327ef94d69a2e0e80890
LookItsCashew <schallkd@mail.uc.edu>
2020-02-20 21:29:04 -0500

    Created Service interface

--- src/main/java/com/trailfinder/service/IHikeService.java
+++ src/main/java/com/trailfinder/service/IHikeService.java
@@ -3,7 +3,11 @@
  */
 package com.trailfinder.service;
 
+import java.util.List;
+
+import com.trailfinder.dto.EventDTO;
 import com.trailfinder.dto.ProfileDTO;
+import com.trailfinder.dto.TrailDTO;
 
 /**
  * @author LookItsCashew
@@ -12,8 +16,32 @@ import com.trailfinder.dto.ProfileDTO;
 public interface IHikeService {
 
 	/*
-	 * Should retrieve a profile
-	 * @return profile a ProfileDTO that contains the profile information of the user
+	 * Should retrieve a user's profile using DAO access
+	 * @return profile a ProfileDTO 
 	 */
-	public ProfileDTO getProfile();
+	public ProfileDTO getProfile(int profileId);
+	
+	/*
+	 * Should save the user's profile using DAO access
+	 * @param profile a ProfileDTO 
+	 */
+	public void saveProfile(ProfileDTO profile);
+	
+	/*
+	 * Should retrieve a list of nearby trails from Google Places
+	 * @return trails a list of TrailDTOs
+	 */
+	public List<TrailDTO> getNearbyTrails();
+	
+	/*
+	 * Should retrieve all events using DAO access
+	 * @return events a list of EventDTOs
+	 */
+	public List<EventDTO> getEvents();
+	
+	/*
+	 * Should create an event using DAO access
+	 * @param profile a ProfileDTO 
+	 */
+	public void createEvent(EventDTO event);
 }

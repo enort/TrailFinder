dc987068c10a643d8e38bc4ef9e806aaa0cb78b8
Konrad Schaller <schallkd@mail.uc.edu>
2020-04-19 20:11:28 -0400

    Writing test for saving an event

--- src/test/java/com/trailfinder/tests/HikeServiceTest.java
+++ src/test/java/com/trailfinder/tests/HikeServiceTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,9 @@ public class HikeServiceTest {
 	IHikeService hikeService;
 	
 	List<EventDTO> events;
+	EventCreatorDTO creator;
+	EventDTO event;
+	EventCreatorDTO creatorCopy;
 	
 	
 	@Test
@@ -53,6 +57,39 @@ public class HikeServiceTest {
 	private void thenListOfEventsWillBeReturned() {
 		// TODO Auto-generated method stub
 		assertEquals(5, events.size());
+	}
+	
+	
+	@Test
+	public void createEvent_CreateEventWithCreatorInfo() throws Exception {
+		givenARegisteredEventHasACreator();
+		whenTheEventIsSaved();
+		thenEventAndCreatorSaveReturnsTrue();
+	}
+
+	private void givenARegisteredEventHasACreator() {
+		// TODO Create an "Event" that will register an event with the creator
+		creator = new EventCreatorDTO("Konrad", "Schaller", "yeetville@ohio.com", "5135555555");
+		//LocalDateTime timeNow = ;
+		event = new EventDTO(5.0, LocalDateTime.now(), LocalDateTime.of(2020, 4, 19, 7, 30), "34.71", "-84.52", creator);
+
+		// Create copy of the creator
+		creatorCopy = event.getEventCreator();
+	}
+
+	private void whenTheEventIsSaved() {
+		// TODO Save the event along with the creator info
+		try {
+			hikeService.createEvent(event);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void thenEventAndCreatorSaveReturnsTrue() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
